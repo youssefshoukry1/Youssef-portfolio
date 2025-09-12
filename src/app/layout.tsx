aac433003259cba899757dbdcbc830dc535935f6
youssefshoukry1 <youssefshoukry1@hotmail.com>
2025-09-12 15:14:54 +0300

    update project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
